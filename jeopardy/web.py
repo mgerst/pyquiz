245c7b45e575684c6cde58fed3fef58eccefa2ae
Matt Gerst <mattgerst@gmail.com>
2017-04-18 15:19:04 -0500

    Add web deployment stuff.

---
+++ jeopardy/web.py
@@ -0,0 +1,14 @@
+import os
+
+from jeopardy import create_app
+
+app = create_app('jeopardy.settings.ProdConfig')
+board = os.environ.get('QUIZ_BOARD', 'board.yml')
+
+bm = app.config.get('BOARD_MANAGER')
+bm.load_boards("board.yml")
+bm.init_boards(board)
+bm.init_boards()
+
+from jeopardy.extensions import socketio
+socketio.run(app)
