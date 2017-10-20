529dd8fdd05cc00bb1cf169fa9db471d0981b5b7
Matt Gerst <mattgerst@gmail.com>
2017-10-20 12:44:51 -0500

    Rename build script folder to not conflict with (ignored) python package build folder.

---
+++ build_scripts/webpack.dev.conf.js
@@ -0,0 +1,46 @@
+var utils = require('./utils')
+var webpack = require('webpack')
+var config = require('../config')
+var merge = require('webpack-merge')
+var path = require('path')
+var baseWebpackConfig = require('./webpack.base.conf')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+var ManifestRevisionPlugin = require('manifest-revision-webpack-plugin')
+
+// add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build_scripts/dev-client'].concat(baseWebpackConfig.entry[name])
+})
+
+module.exports = merge(baseWebpackConfig, {
+  entry: {
+    app: ['webpack-hot-middleware/client?path=http://localhost:8080/__webpack_hmr&timeout=20000',]
+  },
+  module: {
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+  },
+  // cheap-module-eval-source-map is faster for development
+  devtool: '#cheap-module-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // // https://github.com/ampedandwired/html-webpack-plugin
+    // new HtmlWebpackPlugin({
+    //   filename: 'index.html',
+    //   template: 'index.html',
+    //   inject: true
+    // }),
+    new FriendlyErrorsPlugin(),
+    new ManifestRevisionPlugin(path.join('manifest.dev.json'), {
+      rootAssetPath: config.build.assetsRoot,
+      ignorePaths: ['/node_modules', '/src']
+    })
+  ]
+})
+
+console.log(module.exports.entry.app);
