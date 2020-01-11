64213e2c8c1b1c986c7bd2853879efcd1bf048ac
Matt Gerst <mattgerst@gmail.com>
2020-01-10 22:44:34 -0600

    Add team selector to daily double.

--- src/components/Double.vue
+++ src/components/Double.vue
@@ -3,7 +3,12 @@
         <h1>Daily Double!</h1>
 
         <div id="dailyadmin" v-if="isAdmin">
-            <input type="text" v-model="team_number" placeholder="Team Number" />
+            <select v-model="team_number" placeholder="Team Number">
+                <option :selected="team_number === null" disabled="true">Team Number</option>
+                <option v-for="team in teamList" :key="team.id" :value="team.id" :selected="team_number == team.id ? true : false">
+                    {{ team.id }} <span v-if="team.name"> - {{ team.name}}</span>
+                </option>
+            </select>
             <input type="text" v-model="wager" placeholder="Wager" />
             <button type="button" id="dailysubmit" @click="makeWager">Start</button>
         </div>
@@ -22,7 +27,7 @@
             }
         },
         computed: {
-            ...mapGetters(['isAdmin', 'isDailyDouble', 'hasWager']),
+            ...mapGetters(['isAdmin', 'isDailyDouble', 'hasWager', 'teamList']),
         },
         methods: {
             makeWager() {
@@ -34,3 +39,10 @@
         }
     };
 </script>
+
+<style scoped>
+    #dailyadmin select, #dailyadmin input, #dailyadmin button {
+        margin-top: .5em;
+        padding: .5em;
+    }
+</style>
