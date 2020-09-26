c5bdbe1fa088c8ba9b637c542d9cd41b0338ef6e
Matt Gerst <mattgerst@gmail.com>
2020-09-26 00:23:30 -0500

    Upgrade webpack

--- build_scripts/webpack.dev.conf.js
+++ build_scripts/webpack.dev.conf.js
@@ -1,12 +1,11 @@
-var utils = require('./utils')
-var webpack = require('webpack')
-var config = require('../config')
-var merge = require('webpack-merge')
-var path = require('path')
-var baseWebpackConfig = require('./webpack.base.conf')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
-var ManifestRevisionPlugin = require('manifest-revision-webpack-plugin')
+const utils = require('./utils')
+const webpack = require('webpack')
+const config = require('../config')
+const { merge } = require('webpack-merge')
+const path = require('path')
+const baseWebpackConfig = require('./webpack.base.conf')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const ManifestRevisionPlugin = require('manifest-revision-webpack-plugin')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
@@ -20,18 +19,8 @@ module.exports = merge(baseWebpackConfig, {
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': config.dev.env
-    }),
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    // // https://github.com/ampedandwired/html-webpack-plugin
-    // new HtmlWebpackPlugin({
-    //   filename: 'index.html',
-    //   template: 'index.html',
-    //   inject: true
-    // }),
     new FriendlyErrorsPlugin(),
     new ManifestRevisionPlugin(path.join('manifest.dev.json'), {
       rootAssetPath: config.build.assetsRoot,
